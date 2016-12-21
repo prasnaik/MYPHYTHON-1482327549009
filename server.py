cccd8d25c9e71c963208b1a8630d354c08543b44
prashant naik <prasnaik@in.ibm.com>
2016-12-21 08:05:00 -0600

    commit

--- server.py
+++ server.py
@@ -9,6 +9,21 @@ except ImportError:
 # Read port selected by the cloud for our application
 PORT = int(os.getenv('PORT', 8000))
 # Change current directory to avoid exposure of control files
+
+uri = "mongodb://admin:FHJWGTYRUNLADKWU@bluemix-sandbox-dal-9-portal.5.dblayer.com:20715,bluemix-sandbox-dal-9-portal.0.dblayer.com:20715/admin?ssl=true""
+
+client = MongoClient()
+
+client = MongoClient(uri)
+print client
+
+db = client.LCHAnlyzer
+print db
+
+ReadData = db.Test
+ReadData_bpk = ReadData.find()
+for doc in ReadData_bpk:
+    print doc
 os.chdir('static')
 
 httpd = Server(("", PORT), Handler)
