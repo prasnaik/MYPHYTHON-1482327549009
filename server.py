4d29d897e064ba267bb2437b11d97de999593e43
prashant naik <prasnaik@in.ibm.com>
2016-12-21 23:41:38 -0600

    commit

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-import os
-try:
+import os  
+import json  
+from pymongo import MongoClient
+
+ try:
   from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
   from SocketServer import TCPServer as Server
 except ImportError:
@@ -9,16 +12,6 @@ except ImportError:
 # Read port selected by the cloud for our application
 PORT = int(os.getenv('PORT', 8000))
 # Change current directory to avoid exposure of control files
-
-uri = "mongodb://admin:FHJWGTYRUNLADKWU@bluemix-sandbox-dal-9-portal.5.dblayer.com:20715,bluemix-sandbox-dal-9-portal.0.dblayer.com:20715/admin?ssl=true""
-
-client = MongoClient()
-
-client = MongoClient(uri)
-print client
-
-db = client.LCHAnlyzer
-print db
 
 ReadData = db.Test
 ReadData_bpk = ReadData.find()
